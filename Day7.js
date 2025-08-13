1119a3878cae18429915a550bc56946f254f0e83
Vishnupriya3942 <priyavishnu1207vp@gmail.com>
2025-08-13 11:30:27 +0530

    Add files via upload

---
+++ Day7.js
@@ -0,0 +1,36 @@
+//print n
+let a=5;
+for(i=1;i<=a;i++)
+{
+    console .log(i)
+}
+//
+
+//multiplication
+let c=5;
+for(i=1;i<=10;i++)
+{
+  mul=c*i;
+  console.log(mul);
+}
+// sum
+let d=5,sum=0;
+for(i=1;i<=d;i++)
+{
+  sum=sum+i;
+}
+console.log(sum);
+//
+//odd even
+let n=10;
+for(let i=1;i<=n;i+2)
+{
+    console.log(i);
+}
+for(let i=2;i<=n;i+2)
+{
+    console.log(i);
+}
+
+
+
